98645c549048b317fb7f5f2e8249af37cce225ba
MdSakifHossain <sakif015sakif@gmail.com>
2025-07-31 12:38:54 +0600

    fix(seo): update broken image link with Unsplash URL

--- src/constants/seoConfig.js
+++ src/constants/seoConfig.js
@@ -4,7 +4,7 @@ const Title = "Cashout Calc*";
 const Description =
   "Easily calculate bKash cash out charges with this simple and accurate tool. Just enter your amount, and instantly see how much will be deducted based on the latest bKash cash out rates. Perfect for users in Bangladesh who want to avoid surprise fees and manage their money better.";
 const ImageLink =
-  "https://images.pexels.com/photos/1602726/pexels-photo-1602726.jpeg?_gl=1*swegll*_ga*NDM0NjQ0MTAwLjE3NTM5MzcyMDE.*_ga_8JE65Q40S6*czE3NTM5NDIyNjMkbzIkZzEkdDE3NTM5NDIyODMkajQwJGwwJGgw";
+  "https://images.unsplash.com/photo-1618232731737-e1cd67c80bc8?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
 const githubUrl = "https://github.com/MdSakifHossain/cashout-calculator";
 
 export const seoConfig = {
