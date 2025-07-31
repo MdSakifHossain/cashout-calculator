e219f683796fe2b46bb155623bf66bd573bcda6c
MdSakifHossain <sakif015sakif@gmail.com>
2025-07-31 11:51:14 +0600

    refactor(seo): adapt seoConfig for project-specific details

---
+++ src/constants/seoConfig.js
@@ -0,0 +1,24 @@
+// src/utils/seoConfig.js
+
+const Title = "Cashout Calc*";
+const Description =
+  "Easily calculate bKash cash out charges with this simple and accurate tool. Just enter your amount, and instantly see how much will be deducted based on the latest bKash cash out rates. Perfect for users in Bangladesh who want to avoid surprise fees and manage their money better.";
+const ImageLink =
+  "https://images.pexels.com/photos/1602726/pexels-photo-1602726.jpeg";
+const githubUrl = "https://github.com/MdSakifHossain/cashout-calculator";
+
+export const seoConfig = {
+  title: Title,
+  description: Description,
+  ogTitle: Title,
+  ogImage: ImageLink,
+  ogUrl: githubUrl,
+  // You can add more specific twitter fields here if needed
+  twitterImage: undefined,
+  // Will fallback to ogTitle or title if undefined
+  twitterTitle: undefined,
+  // Will fallback to description if undefined
+  twitterDescription: undefined,
+  // Will fallback to ogUrl if undefined
+  twitterUrl: undefined,
+};
